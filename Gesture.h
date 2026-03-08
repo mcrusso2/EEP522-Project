23e0a911206cee1d6b32bd86a5ac2465f569f7fc
mcrusso2 <mattru.business@gmail.com>
2026-03-07 20:43:33 -0800

    I2C related

--- Gesture.h
+++ Gesture.h
@@ -3,7 +3,6 @@
 #include <Wire.h>
 #include <SPI.h>
 #include "paj7620.h"
-#include "pag7660.h"
 
 #define GESTURE_SPI_USELESS 0xff
 
@@ -27,24 +26,4 @@ class paj7620 : public Pixart_Gesture
 
    private:
       bool setReportMode(uint8_t reportMode);
-};
-
-
-class pag7660 : public Pixart_Gesture
-{
-   public:
-      pag7660(int mode = GESTURE_COMBINED_MODE, bool addrChanged = false) {
-         gestureMode = mode;
-         i2c_addr = addrChanged ? PAG7660_I2C_ADDR2 : PAG7660_I2C_ADDR;
-      }
-      bool init(uint8_t cs = GESTURE_SPI_USELESS);
-      bool getResult(pag7660_gesture_t& res);
-      bool getOutput(pag7660_out_t& out);
-
-      int getGestureMode();
-      int nextGestureMode();
-
-   private:
-      uint8_t gestureMode;
-      pag7660_out_t regToOutput(const pag7660_reg_out_t& reg);
 };
--- ISR.c
+++ ISR.c
@@ -28,6 +28,9 @@
 #define PIN13 13
 
 #define SENSOR_ADDR 0x73
+#define PAJ7620_REG_BANK_SEL 0xEF
+#define INIT_REG_ARRAY_SIZE (sizeof(initRegisterArray)/sizeof(initRegisterArray[0]))
+
 
 
 // How much time a change must be since the last in order to count as a change
@@ -84,6 +87,78 @@ void handler(int signum) {
 }
 
 
+const uint8_t initRegisterArray[][2] = {
+    // BANK 0
+    {0xEF,0x00}, {0x37,0x07}, {0x38,0x17}, {0x39,0x06}, {0x42,0x01}, 
+    {0x46,0x2D}, {0x47,0x0F}, {0x48,0x3C}, {0x49,0x00}, {0x4A,0x1E}, 
+    {0x4C,0x20}, {0x51,0x10}, {0x5E,0x10}, {0x60,0x27}, {0x80,0x42}, 
+    {0x81,0x44}, {0x82,0x04}, {0x8B,0x01}, {0x90,0x06}, {0x95,0x0A}, 
+    {0x96,0x0C}, {0x97,0x05}, {0x9A,0x14}, {0x9C,0x3F}, {0xA5,0x19}, 
+    {0xCC,0x19}, {0xCD,0x0B}, {0xCE,0x13}, {0xCF,0x64}, {0xD0,0x21}, 
+    // BANK 1
+    {0xEF,0x01}, {0x02,0x0F}, {0x03,0x10}, {0x04,0x02}, {0x25,0x01},
+    {0x27,0x39}, {0x28,0x7F}, {0x29,0x08}, {0x3E,0xFF}, {0x5E,0x3D}, 
+    {0x65,0x96}, {0x67,0x97}, {0x69,0xCD}, {0x6A,0x01}, {0x6D,0x2C}, 
+    {0x6E,0x01}, {0x72,0x01}, {0x73,0x35}, {0x77,0x01}, {0xEF,0x00},
+};
+
+int paj7620_readReg(int fd, uint8_t reg) {
+    return wiringPiI2CReadReg8(fd, reg);
+}
+
+int paj7620_writeReg(int fd, uint8_t reg, uint8_t val) {
+    return wiringPiI2CWriteReg8(fd, reg, val);
+}
+
+
+int paj7620_init(int fd) {
+    delay(10);
+    paj7620_writeReg(fd, 0xFF, 0x00);
+    delay(50);
+
+    // Check ID
+    if (paj7620_readReg(fd, 0x01) != 0x76 ||
+        paj7620_readReg(fd, 0x00) != 0x20) {
+        printf("PAJ7620 ID mismatch\n");
+        return -1;
+    }
+
+    // Load initialization table
+    for (int i = 0; i < INIT_REG_ARRAY_SIZE; i++) {
+        paj7620_writeReg(fd, initRegisterArray[i][0],
+                             initRegisterArray[i][1]);
+        delay(1);
+    }
+
+    // Set gesture mode (NEAR_240FPS)
+    paj7620_setReportMode(fd, 2);
+
+    return 0;
+}
+
+int paj7620_setReportMode(int fd, uint8_t reportMode) {
+    uint8_t regIdleTime = 0;
+
+    // Switch to Bank 1
+    paj7620_writeReg(fd, PAJ7620_REG_BANK_SEL, 1);
+
+    switch (reportMode) {
+        case 0: regIdleTime = 53;  break;   // FAR_240FPS
+        case 1: regIdleTime = 183; break;   // FAR_120FPS
+        case 2: regIdleTime = 18;  break;   // NEAR_240FPS
+        case 3: regIdleTime = 148; break;   // NEAR_120FPS
+        default: return -1;
+    }
+
+    paj7620_writeReg(fd, 0x65, regIdleTime);
+
+    // Back to Bank 0
+    paj7620_writeReg(fd, PAJ7620_REG_BANK_SEL, 0);
+
+    return 0;
+}
+
+
 int main(void) {
 
 
@@ -126,6 +201,13 @@ int main(void) {
 	}
 	printf("I2C communication successfully setup.\n");
 
+
+	if (paj7620_init(sensor) < 0) {
+        printf("PAJ7620 init failed.\n");
+        return -1;
+    }
+
+    printf("Gesture sensor initialized.\n");
 
 
 	//get current directory
